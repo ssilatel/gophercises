19e527f3b184cdd4ed9900ddc25ad505c6573eb1
ssilatel <setaxe25@gmail.com>
2023-08-17 14:42:49 +0200

    Added a timer

--- exercise-1/main.go
+++ exercise-1/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -26,6 +27,7 @@ func parseLines(lines [][]string) []problem {
 }
 
 func main() {
+	totalTime := flag.Int("t", 30, "Total time for the quiz")
 	filename := flag.String("f", "problems.csv", "Path to the CSV file for the quiz")
 	flag.Parse()
 
@@ -44,15 +46,27 @@ func main() {
 
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*totalTime) * time.Second)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	var correct int
+	var input string
 	for i, p := range problems {
 		fmt.Printf("Problem %d: %s\n", i+1, p.q)
-		scanner.Scan()
-		if scanner.Text() == p.a {
-			correct++
+		inputCh := make(chan string)
+		go func() {
+			scanner.Scan()
+			input = scanner.Text()
+			inputCh <- input
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou answered %d out of %d questions correctly.\n", correct, len(problems))
+			return
+		case input := <-inputCh:
+			if input == p.a {
+				correct++
+			}
 		}
 	}
-
-	fmt.Printf("\nYou answered %d out of %d questions correctly.\n", correct, len(problems))
 }
